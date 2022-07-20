429673dbd1185d88842e764f541e346c4c204c39
leandro47 <leandrosilva47@live.com>
2022-07-19 21:30:52 -0300

    add lib for styles

---
+++ frontend/src/components/ButtonComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <va-button> Button </va-button>
+</template>
+
+<script>
+  export default {
+    name: 'ButtonComponent'
+  }
+</script>
+
+  <style>
+  .app {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  </style>
